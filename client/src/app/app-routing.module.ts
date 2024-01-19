7b039a6fd9b711c401c4341d151c9f38714afb4e
o.hrachov <ol.grachov@gmail.com>
2024-01-19 16:29:14 +0200

    add: add header template

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { DishesPageComponent } from './pages/dishes-page/dishes-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { FavoritesPageComponent } from './pages/favorites-page/favorites-page.component';
 
 const routes: Routes = [
   {
@@ -12,6 +15,18 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'dishes',
+    component: DishesPageComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfilePageComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesPageComponent,
   },
   {
     path: 'signup',
---
+++ client/src/app/pages/dishes-page/dishes-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dishes-page',
+  templateUrl: './dishes-page.component.html',
+  styleUrls: ['./dishes-page.component.scss']
+})
+export class DishesPageComponent {
+
+}
